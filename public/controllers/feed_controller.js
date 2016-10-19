2a8d69aad6e81196de52731c80acc80fb37eccee
Christian Zamora <christian.j.zamora@gmail.com>
2016-10-18 18:10:15 -0700

    push glidejs update for Adam

--- public/controllers/feed_controller.js
+++ public/controllers/feed_controller.js
@@ -1,17 +1,36 @@
 (function() {
-  'use strict';
+  "use strict";
 
-  angular.module('superfanApp')
-  .controller('FeedController', FeedController);
-
-  FeedController.$inject = ['$http', 'twitterDataService'];
-
-  function FeedController($http, twitterDataService) {
-    var vm = this;
-
-    twitterDataService.getBearerToken();
+  angular
+    .module("superfanApp")
+    .controller("FeedController", FeedController);
 
 
-  }
+  FeedController.$inject = ['$http', 'twitterDataService', '$auth', '$state'];
+
+
+    function getTweets($http, twitterDataService, $auth, $state) {
+
+      var vm = this;
+
+      var params = {
+        action: 'user_timeline',
+        user: $scope.username
+      };
+
+      // create Tweet data resource
+      $scope.tweets = $resource('/tweets/:action/:user', params);
+
+      // GET request using the resource
+      $scope.tweets.query( { }, function (res) {
+
+        $scope.tweetsResult = $scope.tweetsResult.concat(res);
+
+        // render tweets with widgets.js
+        $timeout(function () {
+          twttr.widgets.load();
+        }, 30);
+      });
+    }
 
 })();
